8100193535bb9e4cfa91f42c9b722873a1552fe5
Phani051 <phani051@gmail.com>
2022-09-02 21:33:36 +0530

    Used map to use data from JSON

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import emojipedia from "./emojipedia";
 import Emojiitem from "./Emojiitem";
 
+function Emoji(emoji) {
+  return (
+    <Emojiitem key={emoji.id}  name={emoji.name} emoji={emoji.emoji} meaning={emoji.meaning} />
+  );
+}
+
 function App() {
   return (
     <div>
@@ -9,13 +15,7 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">
-        
-        <Emojiitem name={emojipedia[0].name} emoji={emojipedia[0].emoji} meaning={emojipedia[0].meaning} />
-        <Emojiitem name={emojipedia[1].name} emoji={emojipedia[1].emoji} meaning={emojipedia[1].meaning} />
-        <Emojiitem name={emojipedia[2].name} emoji={emojipedia[2].emoji} meaning={emojipedia[2].meaning} />
-        
-      </dl>
+      <dl className="dictionary">{emojipedia.map(Emoji)}</dl>
     </div>
   );
 }
